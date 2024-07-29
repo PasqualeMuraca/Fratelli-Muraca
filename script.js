a3d1f75886611aca2bcaee3bd6c0e7865576f16f
Pasquale Muraca <140654999+pasqualemuraca-uni@users.noreply.github.com>
2024-07-29 11:09:34 +0000

    products working

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-// fill an array of products taken from prodotti.json
-let products = [];
-fetch('products.json')
-    .then(response => response.json())
-    .then(data => {
-        products = data;
-    });
+// Function to load products from JSON file
+async function loadProducts() {
+    try {
+        const response = await fetch('products.json');
+        if (!response.ok) {
+            throw new Error('Error in the request: ' + response.status);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
+}
 
+// main
+document.addEventListener('DOMContentLoaded', async function() {
+    var products = await loadProducts();
 
-document.addEventListener('DOMContentLoaded', function() {
     // for each product append the information with li in a ul with id products_ul
     let products_list = document.getElementById('products_ul');
     products.forEach(product => {
         let li = document.createElement('li');
-        console.log(product);
         li.innerHTML = `
             <h2>${product.name}</h2>
             <p>Prezzo: ${product.price}€</p>
@@ -21,5 +29,5 @@ document.addEventListener('DOMContentLoaded', function() {
         `;
         products_list.appendChild(li);
     });
-});
 
+});
